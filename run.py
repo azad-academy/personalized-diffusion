a9110cf0c24b6a75301e22e50306f7133ed57c28
J. Rafid Siddiqui <76820965+azadlab@users.noreply.github.com>
2022-10-13 16:56:46 +0200

    added scripts

---
+++ run.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+if MODEL_PATH == "":
+    MODEL_PATH = "/content/personalized-diffusion/models/pegasus.ckpt"
+
+os.system("nohup lt --port 1111 > srv.txt 2>&1 &")
+time.sleep(2)
+os.system("grep -o 'https[^ ]*' /content/srv.txt >srvr.txt")
+time.sleep(2)
+srv = getoutput('cat /content/srvr.txt')
+
+os.system("sed -i '1037s@.*@            self.server_name = {srv[8:]}@' /usr/local/lib/python3.7/dist-packages/gradio/blocks.py")
+os.system("sed -i '1039s@.*@            self.server_port = 443@' /usr/local/lib/python3.7/dist-packages/gradio/blocks.py")
+os.system("sed -i '1043s@.*@            self.protocol = https@' /usr/local/lib/python3.7/dist-packages/gradio/blocks.py")
+          
+os.system("sed -i '13s@.*@    PUBLIC_SHARE_TRUE: [32mConnected,@' /usr/local/lib/python3.7/dist-packages/gradio/strings.py")
+  
+os.system("rm /content/srv.txt")
+os.system("rm /content/srvr.txt")
+
+
+with capture.capture_output() as cap:
+  os.system("cd /content/personalized-diffusion/stable-diffusion/")
+
+os.system("python /content/personalized-diffusion/stable-diffusion-webui/webui.py --disable-safe-unpickle --ckpt {MODEL_PATH}")
---
+++ setup_pre-models.py
@@ -0,0 +1,77 @@
+import os
+from IPython.display import clear_output
+from subprocess import getoutput
+from IPython.utils import capture
+import time
+ 
+         
+with capture.capture_output() as cap:
+    
+    os.system("git clone https://github.com/CompVis/stable-diffusion")
+    os.system("git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui")
+    os.system("cd /content/personalized-diffusion/stable-diffusion-webui/")
+    os.system("mkdir -p cache/{huggingface,torch}")
+    os.system("cd /content/")
+    os.system("ln -s /content/personalized-diffusion/stable-diffusion-webui/cache/huggingface ../root/.cache/")
+    os.system("ln -s /content/personalized-diffusion/stable-diffusion-webui/cache/torch ../root/.cache/")
+
+
+with capture.capture_output() as cap:  
+  if not os.path.exists('/content/personalized-diffusion/stable-diffusion/src/k-diffusion/k_diffusion'):
+    os.system("mkdir /content/personalized-diffusion/stable-diffusion/src")
+    os.system("cd /content/personalized-diffusion/stable-diffusion/src")
+    os.system("git clone https://github.com/CompVis/taming-transformers")
+    os.system("git clone https://github.com/openai/CLIP")
+    os.system("mv /content/personalized-diffusion/stable-diffusion/src/CLIP /content/personalized-diffusion/stable-diffusion/src/clip")
+    os.system("git clone https://github.com/TencentARC/GFPGAN")
+    os.system("mv  /content/personalized-diffusion/stable-diffusion/src/GFPGAN/gfpgan /content/personalized-diffusion/stable-diffusion-webui")
+    os.system("git clone https://github.com/salesforce/BLIP")
+    os.system("mv  /content/personalized-diffusion/stable-diffusion/src/BLIP /content/personalized-diffusion/stable-diffusion/src/blip")
+    os.system("git clone https://github.com/sczhou/CodeFormer")
+    os.system("mv  /content/personalized-diffusion/stable-diffusion/src/CodeFormer /content/personalized-diffusion/stable-diffusion/src/codeformer")
+    os.system("git clone https://github.com/xinntao/Real-ESRGAN")
+    os.system("mv  /content/personalized-diffusion/stable-diffusion/src/Real-ESRGAN/ /content/personalized-diffusion/stable-diffusion/src/realesrgan")
+    os.system("git clone https://github.com/crowsonkb/k-diffusion.git")
+    os.system("cp -r /content/personalized-diffusion/stable-diffusion/src/k-diffusion/k_diffusion /content/personalized-diffusion/stable-diffusion-webui")
+    os.system("git clone https://github.com/Hafiidz/latent-diffusion")
+    os.system("cp -r  /content/personalized-diffusion/stable-diffusion/ldm /content/personalized-diffusion/stable-diffusion-webui/")
+
+
+with capture.capture_output() as cap:
+  if not os.path.exists('/usr/local/lib/python3.7/dist-packages/gradio-3.4b3.dist-info'):
+    os.system("cd /content/")
+    os.system("wget https://github.com/TheLastBen/fast-stable-diffusion/raw/main/Dependencies/Dependencies_AUT.1")
+    os.system("wget https://github.com/TheLastBen/fast-stable-diffusion/raw/main/Dependencies/Dependencies_AUT.2
+    os.system("mv Dependencies_AUT.1 Dependencies_AUT.7z.001")
+    os.system("mv Dependencies_AUT.2 Dependencies_AUT.7z.002")
+    os.system("7z x Dependencies_AUT.7z.001")
+    time.sleep(2)
+    os.system("rm -r /content/usr/local/lib/python3.7/dist-packages/transformers")
+    os.system("rm -r /content/usr/local/lib/python3.7/dist-packages/transformers-4.19.2.dist-info")
+    os.system("rm -r /content/usr/local/lib/python3.7/dist-packages/diffusers")
+    os.system("rm -r /content/usr/local/lib/python3.7/dist-packages/diffusers-0.3.0.dist-info")
+    os.system("rm -r /content/usr/local/lib/python3.7/dist-packages/accelerate")
+    os.system("rm -r /content/usr/local/lib/python3.7/dist-packages/accelerate-0.12.0.dist-info")    
+    os.system("cp -r /content/usr/local/lib/python3.7/dist-packages /usr/local/lib/python3.7/")
+    os.system("rm -r /content/usr")
+    os.system("rm Dependencies_AUT.7z.001")
+    os.system("rm Dependencies_AUT.7z.002")
+    os.system("cd /content/personalized-diffusion/stable-diffusion-webui/ldm/modules")
+    os.system("wget -O attention.py https://raw.githubusercontent.com/TheLastBen/fast-stable-diffusion/main/precompiled/attention.py")
+    os.system("cd /content/personalized-diffusion/stable-diffusion-webui/modules")
+    os.system("wget -O paths.py https://raw.githubusercontent.com/TheLastBen/fast-stable-diffusion/main/AUTOMATIC1111_files/paths.py")
+
+with capture.capture_output() as cap: 
+  if not os.path.exists('/tools/node/bin/lt'):
+    os.system("npm install -g localtunnel")
+
+#with capture.capture_output() as cap: 
+  #os.system("cd /content/personalized-diffusion/stable-diffusion-webui/")
+  #time.sleep(1)
+  #os.system("wget -O webui.py https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.py")
+  #os.system("sed -i 's@gpu_call).*@gpu_call) \n        demo.queue(concurrency_count=111500)@' /content/personalized-diffusion/stable-diffusion-webui/webui.py")
+
+#with capture.capture_output() as cap: 
+  #os.system("cd /content")
+
+
